a40bd65ab37cbd4e18e02badeefcb1ec1fe1cb59
karasidnez <karasdnez@gmail.com>
2026-04-08 11:20:38 +0300

    Add repository Timonin

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,3 +1,29 @@
 void main() {
   print('Hello world');
+  String name = 'Ринат';
+  String? name2 = null;
+  var count = 0;
+  var title = "Учить Rust";
+  var x = 5;
+  // x = "hello";
+  final id;
+  id = 5;
+  const appName = "TodoApp";
+  print('$name, $name2, ${count + 5}, $title, $appName');
+  List<String> tags = ['Хых','Хах'];
+  Map<String, dynamic> data = {"key" : 'value','num': 10};
+  Set<int> numbers = {1,2,3,4,1};
+  String greet(String name) => 'Hello $name';
+  printTodo(title: "Купить поесть");
+  printTodo(title: "Сделать домашку", done: true);
+}
+void printTodo({required String title, bool done = false}){
+  print('${done ? '+' : '-'}$title');
+}
+
+class Todo {
+  final int id;
+  String title;
+  bool isDone;
+  Todo({required this.id, required this.title, this.isDone = false});
 }
---
+++ lib/todo.dart
@@ -0,0 +1,20 @@
+class Todo {
+  static int _counter = 0;
+  final id;
+  String title;
+  bool isDone;
+  Todo(this.title) : id ++_coonter, isDone = false{
+  @override
+    String toString(){
+      String status;
+      if(isDone) {
+        status = '[x]';
+      } else {
+        status = '[ ]';
+      }
+      return '$status $id. $title';
+
+    }
+  }
+
+}
